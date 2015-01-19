8b5d9b5b79766d1a0116e64b27acd0cd1b14fee0
mr-georgebaker <georgebaker1@hotmail.com>
2015-01-19 21:12:22 +0100

    Update py_error.py

--- py_error.py
+++ py_error.py
@@ -13,34 +13,81 @@ class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
 
-        self.frame = tk.Frame(self)
-        self.frame.pack()
-        self.bottomframe = tk.Frame(self)
-        self.bottomframe.pack(side='bottom')
+        self.title('Fehlerrechnung')
 
-        self.vars = {}
+        self.formula = tk.Entry(self)
+        self.formula.grid(row=0, column=0)
 
-        self.formula = tk.Entry(self.frame, width=80)
-        self.formula.pack(side='left', fill='x')
-
-        self.analyze_button = tk.Button(self.frame, text='Analyze Formula',
+        self.analyze_button = tk.Button(self, text='Analyze Formula',
                                         command=self.analyze_formula)
-        self.analyze_button.pack(side='left')
+        self.analyze_button.grid(row=0, column=1)
+
+        self.calculate_button = tk.Button(self, text='Calculate',
+                                          command=self.calculate_value)
+        self.calculate_button.grid(row=0, column=2)
+
+        self.entry_val = []
+        self.entry_unc = []
+        self.label = []
 
     def analyze_formula(self):
-        """ Analyzes the formula with sympy and py_expression_eval """
+        """ Parses the formula using sympy and py_expression_eval """
         formula_raw = self.formula.get()
         formula_1 = parser.parse(formula_raw)
-        formula_2 = sp.sympify(formula_raw)
-        variables = formula_1.variables()
-        self.update_UI(variables)
+        self.formula_2 = sp.sympify(formula_raw)
+        self.variables = formula_1.variables()
+        self.update_UI(self.variables)
 
     def update_UI(self, variables):
-        """ Updates the UI to enter the values and uncertainties """
+        """ Adds to the UI entry-fields for values and uncertainties"""
         for i in range(len(variables)):
-            self.vars[i] = tk.StringVar()
-            self.entry = ttk.Entry(self.bottomframe, textvariable=self.vars[i])
-            self.entry.pack(side='bottom', fill='x')
+            self.entry_val.append(ttk.Entry(self))
+            self.entry_val[i].grid(row=i+2, column=1)
+            self.entry_unc.append(ttk.Entry(self))
+            self.entry_unc[i].grid(row=i+2, column=2)
+            self.label.append(tk.Label(self, text=self.variables[i]))
+            self.label[i].grid(row=i+2, column=0)
+            self.label_val = tk.Label(self, text='Value')
+            self.label_val.grid(row=1, column=1)
+            self.label_unc = tk.Label(self, text='Uncertainty')
+            self.label_unc.grid(row=1, column=2)
+
+    def calculate_value(self):
+        """ Calculates the solution """
+        self.dic_value = {}
+        self.differentiate(self.formula_2, self.variables)
+        for i in range(len(self.variables)):
+            self.dic_value.update({self.variables[i]:self.entry_val[i].get()})
+        self.solution = self.formula_2.evalf(subs=self.dic_value)
+        self.calculate_error()
+        self.present_solution()
+
+    def calculate_error(self):
+        """ Calculates the error according to Gaussian error propagation"""
+        self.error = 0
+        for i in range(len(self.diff_list)):
+            value_diff = self.diff_list[i].evalf(subs=self.dic_value)
+            value_diff *= float(self.entry_unc[i].get())
+            value_diff = value_diff**2
+            self.error += value_diff
+        self.error = sp.sqrt(self.error)
+        
+    def differentiate(self, formula, variables):
+        """ Differentiates the formula with respect to all variables """
+        self.diff_list = []
+        for element in variables:
+            sym = sp.sympify(element)
+            differ = sp.diff(formula, sym)
+            self.diff_list.append(differ)
+            
+    def present_solution(self):
+        """ Presents the solution in a canvas """
+        solution_final = str(round(self.solution, 5)) + " +/- " + str(round(self.error, 5))
+        self.canvas = tk.Canvas(self, width=200, height=50)
+        self.canvas.grid(row=100, column=1)
+        canvas_id = self.canvas.create_text(10,10, anchor='nw')
+        self.canvas.itemconfig(canvas_id, text=solution_final)
+        self.canvas.insert(canvas_id, 12, 'new')
 
 def main():
     app = App()
