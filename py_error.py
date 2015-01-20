7969acfcf6d86dcaf63974a534e6da3035b32dd4
mr-georgebaker <georgebaker1@hotmail.com>
2015-01-20 20:42:00 +0100

    Added greek letter support

--- py_error.py
+++ py_error.py
@@ -7,7 +7,10 @@ import sympy as sp
 from sympy.abc import *
 from py_expression_eval import Parser
 
+from constants import REPLACE_DIC
+
 parser = Parser()
+sp.init_printing()
 
 class App(tk.Tk):
     def __init__(self):
@@ -15,24 +18,27 @@ class App(tk.Tk):
 
         self.title('Fehlerrechnung')
 
-        self.formula = tk.Entry(self)
+        self.formula = ttk.Entry(self)
         self.formula.grid(row=0, column=0)
 
-        self.analyze_button = tk.Button(self, text='Analyze Formula',
+        self.analyze_button = ttk.Button(self, text='Analyze Formula',
                                         command=self.analyze_formula)
         self.analyze_button.grid(row=0, column=1)
 
-        self.calculate_button = tk.Button(self, text='Calculate',
+        self.calculate_button = ttk.Button(self, text='Calculate',
                                           command=self.calculate_value)
         self.calculate_button.grid(row=0, column=2)
 
     def analyze_formula(self):
         """ Parses the formula using sympy and py_expression_eval """
-        formula_raw = self.formula.get()
-        self.formula_1 = parser.parse(formula_raw)
-        self.formula_2 = sp.sympify(formula_raw)
-        self.variables = self.formula_1.variables()
-        self.update_UI(self.variables)
+        try:
+            formula_raw = self.formula.get()
+            self.formula_1 = parser.parse(formula_raw)
+            self.formula_2 = sp.sympify(formula_raw)
+            self.variables = self.formula_1.variables()
+            self.update_UI(self.variables)
+        except Exception:
+            self.formula.insert(0, "Please use a different formula")
 
     def update_UI(self, variables):
         """ Adds to the UI entry-fields for values and uncertainties"""
@@ -46,12 +52,13 @@ class App(tk.Tk):
         self.entry_val = []
         self.entry_unc = []
         self.label = []
+        self.nice_greek = [REPLACE_DIC.get(item,item) for item in self.variables]
         for i in range(len(variables)):
             self.entry_val.append(ttk.Entry(self))
             self.entry_val[i].grid(row=i+2, column=1)
             self.entry_unc.append(ttk.Entry(self))
             self.entry_unc[i].grid(row=i+2, column=2)
-            self.label.append(tk.Label(self, text=self.variables[i]))
+            self.label.append(tk.Label(self, text=self.nice_greek[i]))
             self.label[i].grid(row=i+2, column=0)
             self.label_val = tk.Label(self, text='Value')
             self.label_val.grid(row=1, column=1)
@@ -61,12 +68,15 @@ class App(tk.Tk):
     def calculate_value(self):
         """ Calculates the solution """
         self.dic_value = {}
-        self.differentiate(self.formula_2, self.variables)
-        for i in range(len(self.variables)):
-            self.dic_value.update({self.variables[i]:self.entry_val[i].get()})
-        self.solution = self.formula_2.evalf(5, subs=self.dic_value)
-        self.calculate_error()
-        self.present_solution()
+        try:
+            self.differentiate(self.formula_2, self.variables)
+            for i in range(len(self.variables)):
+                self.dic_value.update({self.variables[i]:self.entry_val[i].get()})
+            self.solution = self.formula_2.evalf(5, subs=self.dic_value)
+            self.calculate_error()
+            self.present_solution()
+        except AttributeError:
+            pass
 
     def calculate_error(self):
         """ Calculates the error according to Gaussian error propagation"""
@@ -76,7 +86,7 @@ class App(tk.Tk):
             value_diff *= float(self.entry_unc[i].get())
             value_diff = value_diff**2
             self.error += value_diff
-        self.error = sp.N(sp.sqrt(self.error), 2)
+        self.error = sp.N(sp.sqrt(self.error), 3)
         
     def differentiate(self, formula, variables):
         """ Differentiates the formula with respect to all variables """
